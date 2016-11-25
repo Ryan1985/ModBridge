e0a2102d3d005b324a71444b8303ba70dbab1328
xieyuliang <xieyuliang1985@126.com>
2016-11-25 12:02:39 +0800

    [add]UploadModbridge

---
+++ ModBridge/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ModBridge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(149, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 76);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "启动";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "计数:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "源SLAVE:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "无";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "目标SLAVE:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(68, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(86, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "无";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.ForeColor = System.Drawing.Color.Red;
+            this.lblInfo.Location = new System.Drawing.Point(45, 205);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 12);
+            this.lblInfo.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 244);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Modbus桥接器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ ModBridge/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Modbus.Device;
+
+namespace ModBridge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private volatile bool runFlag = true;
+        private long readByteCnt = 0;
+        private string[] sourceParams;
+        private string[] targetParams;
+        private const int MAX_PACKAGE_LENGTH = 123;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ThreadPool.QueueUserWorkItem((o) =>
+                {
+                    while (runFlag)
+                    {
+                        try
+                        {
+                            var sourceClient = new TcpClient();
+                            sourceClient.Connect(IPAddress.Parse(sourceParams[0]),
+                                int.Parse(sourceParams[1]));
+
+                            var sourceMaster =
+                                ModbusIpMaster.CreateIp(sourceClient
+                                    );
+
+                            var targetClient = new TcpClient();
+                            targetClient.Connect(IPAddress.Parse(targetParams[0]), int.Parse(targetParams[1]));
+
+
+                            var targetMaster =
+                                ModbusIpMaster.CreateIp(targetClient);
+
+                            while (runFlag)
+                            {
+                                var totalLength = ushort.Parse(sourceParams[3]);
+                                ushort startLengthFloat = 0;
+                                do
+                                {
+                                    ushort byteCntToSend = totalLength;
+                                    if (totalLength > MAX_PACKAGE_LENGTH)
+                                    {
+                                        byteCntToSend = MAX_PACKAGE_LENGTH;
+                                    }
+
+                                    var readResult =
+                                        sourceMaster.ReadHoldingRegisters(
+                                            (ushort) (ushort.Parse(sourceParams[2]) + startLengthFloat),
+                                            byteCntToSend);
+                                    targetMaster.WriteMultipleRegisters(
+                                        (ushort) (ushort.Parse(targetParams[2]) + startLengthFloat), readResult);
+                                    readByteCnt += readResult.Length;
+                                    startLengthFloat += (ushort) readResult.Length;
+                                    totalLength -= (ushort) readResult.Length;
+
+                                    try
+                                    {
+                                        Invoke(new Action<long>(l =>
+                                        {
+                                            try
+                                            {
+                                                if (l == long.MaxValue)
+                                                {
+                                                    l = 0;
+                                                }
+                                                label1.Text = l.ToString();
+                                                lblInfo.Text = string.Empty;
+                                            }
+                                            catch
+                                            {
+
+                                            }
+                                        }), readByteCnt);
+                                    }
+                                    catch
+                                    {
+                                    }
+                                } while (totalLength > 0);
+                                Thread.Sleep(100);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            lblInfo.Text = ex.Message;
+                        }
+                        Thread.Sleep(30000);
+                    }
+                });
+                button1.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            runFlag = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            sourceParams = ConfigurationManager.AppSettings["SourceSlave"].Split(',');
+            targetParams = ConfigurationManager.AppSettings["TargetSlave"].Split(',');
+
+            label4.Text = string.Format(@"读取 {0}:{1}，起始地址：{2}，长度：{3}", sourceParams[0], sourceParams[1], sourceParams[2],
+                sourceParams[3]);
+            label7.Text = string.Format(@"读取 {0}:{1}，起始地址：{2}，长度：{3}", targetParams[0], targetParams[1], targetParams[2],
+                targetParams[3]);
+
+
+        }
+    }
+}
